7fdb77d5d38f214f608b5cca86962f0b40728012
Joseph Lopez <225996877+predictablyunpredictable789@users.noreply.github.com>
2025-09-02 06:01:55 +0000

    reviewed previous code: updated code (still not working)

--- w4/app.js
+++ w4/app.js
@@ -1,16 +1,35 @@
-//something goes here, possibly console.log()
+console.log('Hello from app.js! Your JavaScript is connected and running!');
 
+const clearForm = document.getElementById('clearFormButton');
+const submitForm = document.getElementById('submitFormButton');
+const textBox = document.getElementById('textBoxInput');
 
-// call form handler
-// prevent page from loading
-// display form submitted in console
-inputName = name
-const formHandler() {
-    event.preventDefault();
-    document.addEventListener('click', submit);
-    document.finalGreet.value() = "Hello, [Name]!"
-    console.log(`Form Submitted`)
-}
+// submit form
+// trying to get form to display input content after submission
+const submitFormButton = function(event) {
+        event.preventDefault();
+        console.log(event);
+        textBox.textContent = `Welcome ${textContent}`;
+        //document.textBoxInput();
+        console.log(`Hello, ${textBoxInput}!`);
+    }
+
+// trying to get text equal to nothing when cleared ()
+const clearFormButton = function(event) {
+    clearForm.reset();
+    textBox.value = "";
+};
+
 
 // link to other doc, unsure what goes here
-DOM
+// reviewed w3
+// add init function
+// added DOMContentLoader
+function init() {
+    console.log('App initialized: DOM is ready! Try submitting the form or clearing it.');
+    document.addEventListener('submit', submitFormButton);
+    document.addEventListener('click', clearFormButton);
+     
+}
+
+document.addEventListener('DOMContentLoaded', init);
