0096559298d9b763fe713580d57df1b5f55e00fe
Joseph Lopez <225996877+predictablyunpredictable789@users.noreply.github.com>
2025-09-02 00:18:41 +0000

    feat: initial attempt at greeting generator

---
+++ w4/app.js
@@ -0,0 +1,16 @@
+//something goes here, possibly console.log()
+
+
+// call form handler
+// prevent page from loading
+// display form submitted in console
+inputName = name
+const formHandler() {
+    event.preventDefault();
+    document.addEventListener('click', submit);
+    document.finalGreet.value() = "Hello, [Name]!"
+    console.log(`Form Submitted`)
+}
+
+// link to other doc, unsure what goes here
+DOM
